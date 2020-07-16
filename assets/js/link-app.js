b4e38fcf90f4e26eae8685e94330ce686aa5f762
Jepri Kusuma <jeprikusuma11@gmail.com>
2020-07-16 20:08:56 +0800

    GIT INIT

---
+++ assets/js/link-app.js
@@ -0,0 +1,23 @@
+const container = document.querySelector(".container"),
+  more = container.querySelector(".link a.more"),
+  back = container.querySelector(".more-content .back a"),
+  loader = document.querySelector(".loader");
+
+more.addEventListener("click", () => {
+  container.classList.add("show-more");
+  container.querySelector(".more-content").style.animation =
+    "1s both sideContentItems";
+  container.querySelector(".main-content").style.animation =
+    "1s both sideContent";
+  container.querySelector("main .link").style.animationDelay = "1s";
+});
+back.addEventListener("click", () => {
+  container.classList.remove("show-more");
+  container.querySelector(".more-content").style.animation =
+    "1s both sideContentItemsBack";
+  container.querySelector(".main-content").style.animation = "1s both sideBack";
+});
+
+function loaderFun() {
+  loader.classList.remove("active");
+}
